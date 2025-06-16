cf2ae4b1602ff1c02ecc7fc35508ddb63a771ce4
Gabriel Lima Dantas <101371170+PeDeLaranjaLima@users.noreply.github.com>
2025-06-16 11:36:52 -0300

    Add files via upload

---
+++ Aula - 02062025 - Exemplo 1 - Testes com funções.js
@@ -0,0 +1,7 @@
+// Aula do dia 02/06/2025 -> Teste com funções
+
+potencia = (num, n)  => num**n
+
+console.log(potencia(2, 2))   // Testar quadrado
+console.log(potencia(4, 1/2)) // Testar raiz
+console.log(potencia(2, -1))  // Testar inverso
